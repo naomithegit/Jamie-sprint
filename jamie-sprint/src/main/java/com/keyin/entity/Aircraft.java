3fb2b17fc830f05f7d03cdbbe6ba18fdd005a834
Cap2711 <chelsey.penton@keyin.com>
2024-10-22 11:07:10 -0230

    getters and setters specified

--- jamie-sprint/src/main/java/com/keyin/entity/Aircraft.java
+++ jamie-sprint/src/main/java/com/keyin/entity/Aircraft.java
@@ -3,27 +3,47 @@ package com.keyin.entity;
 import javax.persistence.*;
 
 @Entity
-    public class Aircraft {
+public class Aircraft {
 
-        @Id
-        @SequenceGenerator(name = "aircraft_sequence", sequenceName = "aircraft_sequence", allocationSize = 1, initialValue = 1)
-        @GeneratedValue(generator = "aircraft_sequence")
-        private long id;
-        private String model;
-        private String manufacturer;
-        public long getId() {
+    @Id
+    @SequenceGenerator(name = "aircraft_sequence", sequenceName = "aircraft_sequence", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(generator = "aircraft_sequence")
+    private long id;
+    private String type;
+    private String airLineName;
+    private int numberOfPassengers;
+
+    // Getters and Setters
+    public Long getId() {
         return id;
     }
 
-        public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-        public String getModel() {
-        return model;
+    public String getType() {
+        return type;
     }
 
-        public void setModel(String model) {
-        this.model = model;
+    public void setType(String type) {
+        this.type = type;
     }
+
+    public String getAirlineName() {
+        return airLineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airLineName = airlineName;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
 }
